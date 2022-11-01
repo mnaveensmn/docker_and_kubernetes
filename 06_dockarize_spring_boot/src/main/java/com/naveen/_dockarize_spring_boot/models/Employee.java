ac1735653b200614c74eaf42a33600e32bcb13c4
naveen.kumar1 <naveen.kumar1@thoughtworks.com>
2022-11-01 23:18:14 +0530

    dockerize spring boot application

--- 06_dockarize_spring_boot/src/main/java/com/naveen/_dockarize_spring_boot/models/Employee.java
+++ 06_dockarize_spring_boot/src/main/java/com/naveen/_dockarize_spring_boot/models/Employee.java
@@ -2,11 +2,22 @@ package com.naveen._dockarize_spring_boot.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
 public class Employee {
 
-    String message;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String email;
+    String country;
 
 }
